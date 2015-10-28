fda60e701430b0c615c2e5ddc5ac8541bf77b6c1
Alex Carlin <carlin@ucdavis.edu>
2015-10-27 18:02:29 -0700

    Added alignments and diffs and associated scripts

--- orders/20151027/align.bash
+++ orders/20151027/align.bash
@@ -1,3 +1,5 @@
 for i in out/*fasta; do
   blastn -subject bagel.fasta -query $i -out ${i/fasta/blast}  
+  blastn -subject bagel.fasta -query $i -outfmt "6 sseq qseq" -out ${i/fasta/aligned} 
+  tblastn -subject $i -query bglb.pep -outfmt "6 qseq sseq" -out ${i/fasta/diff} 
 done 
