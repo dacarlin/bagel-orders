b37e292b27b42f3e4edd0f283425fd81c44946de
Alex Carlin <carlin@ucdavis.edu>
2015-12-15 15:55:46 -0800

    Added sequencing results for Dec 03 order

--- orders/20151027/diff.py
+++ orders/20151027/diff.py
@@ -1,16 +1,19 @@
 from glob import glob 
 import pandas 
+import json 
 
 df = pandas.read_csv( 'transcriptic_mapping.csv' )  
 df.set_index( 'mutant', inplace=True )  
 
+picks = { } 
 for fil in glob( 'out/*diff' ):
   with open( fil ) as fn:
     sub, que = fn.readlines()[0].split( '\t' )   
     d = '+'.join( [ '{}{}{}'.format(i, n+1, j).lower() for (n, (i, j)) in enumerate( zip( sub, que ) ) if i != j ] ) 
     if fil.split( '/' )[1].split( '.' )[0].split( '-' )[0] == d:
-     
       thing = fil.split( '/' )[1].split( '.' )[0]
-      print thing + '\t' +  df.loc[ thing ].well 
+      picks.update( { d: df.loc[ thing ].well } ) 
+
+print json.dumps( picks, indent=2 )
     
 
---
+++ orders/20151102/autoprotocol_lib/transcriptic_instruction.py
@@ -0,0 +1,43 @@
+from autoprotocol.instruction import Instruction
+
+'''
+    :copyright: 2015 by The Autoprotocol Development Team, see AUTHORS
+        for more details.
+    :license: BSD, see LICENSE for more details
+
+'''
+
+
+class Miniprep(Instruction):
+    """
+    Perform mini-prep on indicated aliquot(s).
+
+    Parameters
+    ----------
+    group_opts : list
+        List of {from: well1, to: well2} instructions
+
+    """
+    def __init__(self, group_opts):
+        super(Miniprep, self).__init__({
+            "op": "miniprep",
+            "groups": group_opts
+        })
+
+
+class Maxiprep(Instruction):
+    """
+    Grow to scale and perform maxi-prep from indicated aliquot(s).
+
+    Parameters
+    ----------
+    group_opts : list
+        List of {from: well1, to: well2} instructions
+
+    """
+    def __init__(self, group_opts):
+        super(Maxiprep, self).__init__({
+            "op": "maxiprep",
+            "groups": group_opts
+        })
+
